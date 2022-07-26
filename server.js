7c6412ad8941ff681e1250d0197938eb93a4e429
alonpatashnik <alonp714@gmail.com>
2022-07-25 17:26:34 -0700

    added read from and read and append functions

--- server.js
+++ server.js
@@ -2,10 +2,31 @@ const express = require('express');
 const path = require('path');
 const PORT = 3001;
 const app = express();
+const uniqid = require('uniqid')
+const fs = require('fs')
+const util = require('util')
+const readFromFile = util.promisify(fs.readFile)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+  const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -14,6 +35,19 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+app.get('/api/notes', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+app.post('/api/notes', (req, res) => {
+    const { noteTitle, NoteContent } = req.body;
+    const newNote = {
+        title: `${noteTitle}`,
+        text: `${NoteContent}`,
+        id: uniqid()
+    };
+    readAndAppend(newNote, './db/db.json');
+})
 
 
 app.listen(PORT, () =>
