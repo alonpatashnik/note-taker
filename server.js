6ed567d1330137b2e9376ed8ee65db6dbf92dbc2
alonpatashnik <alonp714@gmail.com>
2022-07-26 15:23:23 -0700

    needed to set port to env variable

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const PORT = 8080;
+const PORT = process.env.PORT || 80;
 const app = express();
 const uniqid = require('uniqid')
 const fs = require('fs')
